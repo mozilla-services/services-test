fffc7dcc132689373234c7b50b9831a2e52a6d48
Peter deHaan <peter@deseloper.com>
2015-06-18 14:10:29 -0700

    Adding install.sh and run.sh scripts

---
+++ e2e/pocket/install.sh
@@ -0,0 +1,12 @@
+# TODO: Download and install latest Firefox browser for Ubuntu.
+# wget https://ftp.mozilla.org/pub/mozilla.org/firefox/releases/latest/linux-x86_64/en-US/firefox-38.0.5.tar.bz2
+# tar -xjvf *.bz2 -C /home/ubuntu
+
+# TODO: karl optimization for downloading latest version without needing to hardcode version numbers. :+1:
+# LATEST_DMG=$(curl -s ftp://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-central/ | fgrep en-US.mac.dmg | awk '{print $9}'
+# curl -# -C - -o $DIR'/LatestNightly.dmg' "ftp://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-central/$LATEST_DMG"
+
+rm -rf services-test
+git clone https://github.com/mozilla-services/services-test
+cd services-test/e2e/pocket
+npm install
