296ab7e13712ab57f761803979df165b861f31a6
Stuart Philp <stuartphilp@gmail.com>
2015-06-19 17:05:50 -0400

    pass project as arguement

--- absearch/test/schema_check.sh
+++ absearch/test/schema_check.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 # Read the config.schema.json and validates against config.json
-python $PROJECT_DIR/setup.py develop
+# $1: github project name
+rm -rf $1
+git clone http://github.com/mozilla-services/$1
+python $1/setup.py develop
 absearch-check
