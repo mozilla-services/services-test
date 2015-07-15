bfc8a16353ba7e4e0ced74f377e7d3cf4e34cd04
Koki Yoshida <kyoshida@MacBook-Pro.local>
2015-07-15 13:01:02 -0700

    Fixed README file not found bug

--- msisdn-gateway/e2e-test/run.sh
+++ msisdn-gateway/e2e-test/run.sh
@@ -6,7 +6,7 @@ python ./setup.py develop
 fab setup || exit 1
 echo "-----------------------------------Setup Complete----------------------------------"
 echo "---------------------------Virtual Environment Activated---------------------------"
-python ./test/control-script.py -a ${ACCOUNT_AUTH} -n ${NUMBERS} -h ${GATEWAY_HOST_NUMBER}
+python ./test/control-script.py -a ${ACCOUNT_AUTH} -n ${NUMBERS} -H ${GATEWAY_HOST_NUMBER}
 echo "------------------------------------Completed Test---------------------------------"
 echo "------------------------------------Begin Teardown---------------------------------"
 fab teardown || exit 1
