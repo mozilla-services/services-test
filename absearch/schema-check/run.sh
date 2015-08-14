e0a8bb057237937acdae9b52d6bf456f5a8aa6b5
Stuart Philp <stuartphilp@gmail.com>
2015-08-14 15:21:02 -0400

    grab username and password for absearch

--- absearch/schema-check/run.sh
+++ absearch/schema-check/run.sh
@@ -2,6 +2,6 @@
 # Read the config.schema.json and validates against config.json
 # The absearch project stores it's config/schema in the absearchdata project
 rm -rf absearchdata
-git clone git@github.com:mozilla-services/absearchdata.git
+git clone https://$(cat /secrets/common.yaml | shyaml get-value GITHUB_USERNAME.value):$(cat /secrets/common.yaml | shyaml get-value GITHUB_PASSWORD.value)@github.com/mozilla-services/absearchdata.git
 make -C absearchdata/scripts/ install
 make -C absearchdata/scripts/ check_config
