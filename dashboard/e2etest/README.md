0656a2518c362ce0a736e9dd8fe1e42789d014f3
Stuart Philp <stuartphilp@gmail.com>
2015-10-22 17:42:23 -0400

    changed over to use id's for elements so they are guaranteed to be correct

--- dashboard/e2etest/README.md
+++ dashboard/e2etest/README.md
@@ -1,15 +1,15 @@
-# Pocket E2E tests
+# Dashboard E2E tests
 
-End-to-end tests for the [Pocket](https://getpocket.com) button in Firefox.
+End-to-end tests for the [Services QA Dashboard](https://github.com/mozilla-services/services-quality-dashboard)
 
 ## How Do I Even?
 ```sh
 $ git clone git@github.com:mozilla-services/services-test.git
-$ cd e2e/pocket/
-$ npm run pocket
+$ cd dashboard/e2etest
+$ npm run dashboard
 ```
 
-The `pocket` npm script will look for the following runtime on your system:
+The `dashboard` npm script will look for the following runtime on your system:
 ```
 --runtime /Applications/Firefox.app/Contents/MacOS/firefox-bin
 ```
--- dashboard/e2etest/run.sh
+++ dashboard/e2etest/run.sh
@@ -1,13 +1,5 @@
 #!/bin/bash +x
 
-# TODO: Download and install latest Firefox browser for Ubuntu.
-# wget https://ftp.mozilla.org/pub/mozilla.org/firefox/releases/latest/linux-x86_64/en-US/firefox-38.0.5.tar.bz2
-# tar -xjvf *.bz2 -C /home/ubuntu
-
-# TODO: karl optimization for downloading latest version without needing to hardcode version numbers. :+1:
-# LATEST_DMG=$(curl -s ftp://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-central/ | fgrep en-US.mac.dmg | awk '{print $9}'
-# curl -# -C - -o $DIR'/LatestNightly.dmg' "ftp://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-central/$LATEST_DMG"
-
 #-------------------
 # Install deps
 #-------------------
