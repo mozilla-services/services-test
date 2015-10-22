373d7923cf9a6e613aba269451a1c172b305aa10
Stuart Philp <stuartphilp@gmail.com>
2015-10-22 11:49:31 -0400

    stub out basic tests for kinto buckets, collections, record, and groups

---
+++ kinto/e2etest/test_kinto_buckets.py
@@ -0,0 +1,38 @@
+import os.path
+import unittest
+
+from mockclient import MockClient
+
+class Kinto_Buckets(unittest.TestCase):
+    """
+        Tests to verify the Kinto buckets function
+        Docs: http://kinto.readthedocs.org/en/latest/api/buckets.html
+    """
+
+    def setUp(self):
+        self.client = MockClient()
+
+    def tearDown(self):
+        self.client = None
+
+    def test_create_bucket(self):
+        raise NotImplementedError("test not implemented")
+    def test_replace_bucket(self):
+        raise NotImplementedError("test not implemented")
+    def test_retrieve_bucket(self):
+        raise NotImplementedError("test not implemented")
+    def test_retrieve_all_buckets(self):
+        """Fetch list of available buckets"""
+        #
+        # auth has to work to dive in API stuff...
+        #
+        # resource = 'buckets'
+        # response = self.client.request(resource)
+        # print(response)
+        # self.assertIn('bucket', response)
+        # self.assertEqual(response['bucket'], 'some thing!')
+        raise NotImplementedError("test not implemented")
+    def test_update_bucket(self):
+        raise NotImplementedError("test not implemented")
+    def test_delete_bucket(self):
+        raise NotImplementedError("test not implemented")
