3dfa71ce7113d59e466f1172eb019c107e5ac33c
Stuart Philp <stuartphilp@gmail.com>
2015-10-26 16:13:53 -0400

    basic auth

--- kinto/e2etest/mockclient.py
+++ kinto/e2etest/mockclient.py
@@ -1,11 +1,16 @@
 import json
-from urllib2 import urlopen
+import base64
+import urllib2
 
 stage_url = "https://kinto.stage.mozaws.net/v1/"
 
 class MockClient(object):
   def request(self, resource):
+    base64string = base64.encodestring('%s:%s' % ("testuser", "abc123")).replace('\n', '')
     url = stage_url + resource
-    response = urlopen(url)
+    request = urllib2.Request(url)
+    request.add_header("Authorization", "Basic %s" % base64string)
+    response = urllib2.urlopen(request)
+    # response = urlopen(url)
     raw_data = response.read().decode('utf-8')
     return json.loads(raw_data)
