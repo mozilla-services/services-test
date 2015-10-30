f7e5f2078e674f3cd1dddffb867802eb9890a50f
Johnny Quest <cloud-services-qa@mozilla.com>
2015-10-30 05:57:33 +0000

    parameterize images 2 (WIP)

--- autopush/e2e-test/tests/pop-notification-NN64.sikuli/pop-notification-NN64.py
+++ autopush/e2e-test/tests/pop-notification-NN64.sikuli/pop-notification-NN64.py
@@ -1,53 +1,63 @@
-NIGHTLY64 = "C:/Program Files/Nightly/firefox.exe -height 768 -width 1024"
-PATH_FIREFOX = NIGHTLY64
-PATH_IMGS = '../images/'
 APP_NAME = 'Nightly'
+#APP_NAME = 'Release'
+NIGHTLY64 = "C:/Program Files/%s/firefox.exe -height 768 -width 1024" % APP_NAME
+PATH_FIREFOX = NIGHTLY64
+
+if APP_NAME == 'Nightly':
+    PATH_IMGS = '../images_nightly64/'
+else:
+    PATH_IMGS = '../images_release32/'
+    print('TBD -> need images for GR32!!')
+    exit(1) 
+    
+
 URL_TEST_PAGE = 'https://pdehaan.github.io/push-notification-test'
 
-IMG_REFRESH_NIGHTLY = PATH_IMGS + "1446181480598.png"  
-# moz foundation
-URL_ENTRY_POINT_1 = Pattern(PATH_IMGS + "1446175705142.png").targetOffset(110,-4)
-#search
-URL_ENTRY_POINT_2 = PATH_IMGS + "1446176591082.png"
-# nightly
-URL_ENTRY_POINT_3 = Pattern(PATH_IMGS + "1446175756881.png").targetOffset(59,0)
-# green padlock
-URL_ENTRY_POINT_4 = Pattern(PATH_IMGS + "1446178093719.png").targetOffset(35,1)
-
-IMG_WIN_MAXIMIZE = PATH_IMGS + "1446160870518.png"
-IMG_ALWAYS_RECEIVE_NOTIFICATIONS = PATH_IMGS + "1446176332222.png"
-IMG_BTN_POP_NOTIFICATION = PATH_IMGS + "1446153249015.png"
-IMG_WIN_POP_NOTIFICATION = PATH_IMGS + "1446161475675.png"
+IMG_REFRESH = PATH_IMGS + "btn_refresh.png" # 
+URL_ENTRY_POINT_1 = Pattern(PATH_IMGS + "url_entry_moz.png").targetOffset(110,-4)
+URL_ENTRY_POINT_2 = PATH_IMGS + "url_entry_search.png" 
+URL_ENTRY_POINT_3 = Pattern(PATH_IMGS + "url_entry_nightly.png").targetOffset(59,0)
+URL_ENTRY_POINT_4 = Pattern(PATH_IMGS + "url_entry_green_padlock.png").targetOffset(59,1)
+IMG_WIN_MAXIMIZE = PATH_IMGS + "win_maximize.png" 
+IMG_ALWAYS_RECEIVE_NOTIFICATIONS = PATH_IMGS + "btn_always_receive.png"
+IMG_BTN_POP_NOTIFICATION = PATH_IMGS + "btn_pop_notification.png" 
+IMG_WIN_POP_NOTIFICATION = PATH_IMGS + "win_pop_notification.png" 
 LINE = '--------------------------'
 
 
+firefox = App(APP_NAME)
 
+def header(label):
+    return '%s\n%s\n%s' % (LINE, label, LINE)
+    
 def setup():
     
-    print('%s\n%s\n%s' % (LINE, 'SETUP', LINE))
-    firefox = App(APP_NAME)
+    print header('SETUP')
     firefox.open(PATH_FIREFOX)
-    if exists(IMG_REFRESH_NIGHTLY):
-        click(IMG_REFRESH_NIGHTLY)
-        click(IMG_REFRESH_NIGHTLY)
+    if exists(IMG_REFRESH):
+        click(IMG_REFRESH)
+        click(IMG_REFRESH)
     firefox.focus()
 
 def teardown():
 
-    print('%s\n%s\n%s' % (LINE, 'TEARDOWN', LINE))
+    #print('%s\n%s\n%s' % (LINE, 'TEARDOWN', LINE))
+    print header('TEARDOWN')
     print('closing browser now')
     firefox.close()
     print('Firefox closed')
     
 setup()
 
+print header('TEST')
+
 if exists(URL_ENTRY_POINT_1):
     print('URL ENTRY POINT: <Mozilla Foundation>')
     click(URL_ENTRY_POINT_1)
-elif(URL_ENTRY_POINT_2):
+elif exists(URL_ENTRY_POINT_2):
     print('URL ENTRY POINT: <Search or enter address>')
     click(URL_ENTRY_POINT_2)
-elif(URL_ENTRY_POINT_3):
+elif exists(URL_ENTRY_POINT_3):
     print('URL ENTRY POINT: <Nightly...>')
     click(URL_ENTRY_POINT_3)
 else:
@@ -71,5 +81,3 @@ waitVanish(IMG_WIN_POP_NOTIFICATION)
 print('TEST COMPLETE!')
 
 teardown()
-
-
