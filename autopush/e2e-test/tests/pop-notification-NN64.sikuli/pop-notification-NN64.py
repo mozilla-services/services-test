62eb71bd0a4566bf763b4482ca63a4804cb4caee
Johnny Quest <cloud-services-qa@mozilla.com>
2015-10-30 04:13:14 +0000

    Make Nightly 64 more resilient

--- autopush/e2e-test/tests/pop-notification-NN64.sikuli/pop-notification-NN64.py
+++ autopush/e2e-test/tests/pop-notification-NN64.sikuli/pop-notification-NN64.py
@@ -1,35 +1,62 @@
 NIGHTLY64 = "C:/Program Files/Nightly/firefox.exe -height 768 -width 1024"
 PATH_FIREFOX = NIGHTLY64
+APP_NAME = 'Nightly'
 URL_TEST_PAGE = 'https://pdehaan.github.io/push-notification-test'
 
-firefox = App('Nightly')
-firefox.open(PATH_FIREFOX)
-sleep(2)
-firefox.focus()
+URL_ENTRY_POINT_1 = Pattern("1446175705142.png").targetOffset(110,-4)
+URL_ENTRY_POINT_2 = "1446176591082.png"
+URL_ENTRY_POINT_3 = Pattern("1446175756881.png").targetOffset(59,0)
+URL_ENTRY_POINT_4 = Pattern("1446178093719.png").targetOffset(35,1)
+
+
+def teardown():
+    print('closing browser now')
+    firefox.close()
+    print('Firefox closed')
+
+def setup():
+    firefox = App(APP_NAME)
+    firefox.open(PATH_FIREFOX)
+    sleep(2)
+    firefox.focus()
+
+setup()
 
 click("1446160870518.png")
 
-click("1446159886063.png")
-
+if exists(URL_ENTRY_POINT_1):
+    print('URL entry point: <Mozilla Foundation>')
+    click(URL_ENTRY_POINT_1)
+elif(URL_ENTRY_POINT_2):
+    print('URL entry point: <Search or enter address>')
+    click(URL_ENTRY_POINT_2)
+elif(URL_ENTRY_POINT_3):
+    print('URL entry point: <Nightly...>')
+    click(URL_ENTRY_POINT_3)
+else:
+    print('URL entry point: <green padlock>')
+    click(URL_ENTRY_POINT_4)
 type(URL_TEST_PAGE + Key.ENTER)
 
 click("1446153249015.png")
 
 sleep(2)
 
-if exists("1446159501996.png"):
-    click("1446159501996.png")
+print('<Always Receive Notifications?>')
+if exists("1446176332222.png"):
+    click("1446176332222.png")
 
-print "pop notification: here?"
+print "pop notification: ???"
 
 wait("1446161475675.png")
+print('pop notification complete!')
 
-print('pop notificaiton: waiting for vanish')
+print('pop notification: waiting for vanish')
 
 waitVanish("1446161475675.png")
 
-print "DONE! --> close browser now"
+print('TEST COMPLETE!')
 
-firefox.close()
+teardown()
 
 
