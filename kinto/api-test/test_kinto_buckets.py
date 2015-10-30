774a263f925ba548b36bd46b1f31bc88ecde0025
Rémy HUBSCHER <rhubscher@mozilla.com>
2015-10-30 11:33:28 +0100

    Fix tests.

--- kinto/api-test/test_kinto_buckets.py
+++ kinto/api-test/test_kinto_buckets.py
@@ -17,7 +17,7 @@ class Kinto_Buckets(unittest.TestCase):
 
     def test_create_bucket(self):
         resource = 'buckets'
-        data = '{"data": {"id": "test_bucket"}}'
+        data = '{"data": {"id": "test_bucket", "foo": "bar"}}'
         response = self.client.post_request(resource, data)
         self.assertIn('data', response)
         self.assertIn('last_modified', response['data'])
@@ -50,29 +50,33 @@ class Kinto_Buckets(unittest.TestCase):
             self.assertIn('last_modified', item)
             self.assertIn('id', item)
 
-    # patch request for update currently broken, see:
-    # https://github.com/mozilla-services/cliquet/issues/516
     def test_update_bucket_no_data(self):
-        # resource = 'buckets/test_bucket'
-        # data = ''
-        # response = self.client.patch_request(resource, data)
-        # self.assertIn('data', response)
-        # self.assertIn('permissions', response)
-        # self.assertIn('last_modified', response['data'])
-        # self.assertIn('id', response['data'])
-        raise NotImplementedError("bucket update currently broken for patch with no data")
+        resource = 'buckets/test_bucket'
+        data = ''
+        response = self.client.patch_request(resource, data, status_code=400)
+        self.assertIn('errno', response)
+        self.assertIn('message', response)
+        self.assertIn('code', response)
+        self.assertIn('error', response)
+        self.assertEqual(response['errno'], 107)
+        self.assertEqual(response['message'], "Provide at least one of data or permissions")
+        self.assertEqual(response['code'], 400)
+        self.assertEqual(response['error'], 'Invalid parameters')
 
     def test_update_bucket_with_data(self):
+        # For now bucket patch only allow permission updates
+        # See kinto#239
         resource = 'buckets/test_bucket'
-        data = '{"data": {"id": "test_update_bucket"}}'
+        data = '{"permissions": {"read": ["basicauth:foobar"]}}'
         response = self.client.patch_request(resource, data)
         self.assertIn('data', response)
         self.assertIn('permissions', response)
         self.assertIn('last_modified', response['data'])
         self.assertIn('id', response['data'])
-        self.assertEqual(response['data']['id'], 'test_update_bucket')
+        self.assertEqual(response['permissions']['read'], ['basicauth:foobar'])
 
     def test_delete_bucket(self):
+        # Create the bucket
         resource = 'buckets'
         data = '{"data": {"id": "delete_bucket"}}'
         response = self.client.post_request(resource, data)
@@ -81,6 +85,7 @@ class Kinto_Buckets(unittest.TestCase):
         self.assertIn('id', response['data'])
         self.assertEqual(response['data']['id'], 'delete_bucket')
 
+        # Delete the bucket
         resource = 'buckets/delete_bucket'
         response = self.client.delete_request(resource)
         self.assertIn('data', response)
