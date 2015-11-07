7ce44cf8f4bada69df8058846a72fc56ee69bcb6
Stuart Philp <stuartphilp@gmail.com>
2015-11-07 09:56:53 -0500

    add readonly mode to test asserts

--- kinto/api-test/mockclient.py
+++ kinto/api-test/mockclient.py
@@ -8,11 +8,23 @@ stage_url = "https://kinto.stage.mozaws.net/v1/"
 class MockClient(object):
 
     def __init__(self):
+        self.readonly = ""
         self.auth_string = base64.b64encode('%s:%s' % ("testuser", "abc123"))
         self.headers = {
             "Authorization": "Basic %s" % self.auth_string,
             "Content-Type": "application/json"
         }
+
+    def is_read_only(self):
+        if not self.readonly or self.readonly == "":
+            self.readonly = self.get_read_only_state()
+        return self.readonly
+
+    def get_read_only_state(self):
+        url = stage_url
+        r = requests.get(url, headers=self.headers)
+        response = json.loads(r.content.decode('utf-8'))
+        return response['settings']['readonly']
 
     def get_request(self, resource, status_code=None):
         url = stage_url + resource
