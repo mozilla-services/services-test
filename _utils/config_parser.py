34c5e4045a6fc02ad2299972adee7c09437cc118
Peter deHaan <peter@deseloper.com>
2015-11-13 16:45:47 -0800

    Add a few try..except blocks to handle errors more gracefully

--- _utils/config_parser.py
+++ _utils/config_parser.py
@@ -2,15 +2,43 @@ import os
 import platform
 import ConfigParser
 
-Config = ConfigParser.ConfigParser()
-Config.read('path_firefox.ini')
 
-system = platform.system().lower().split('_').pop(0)
+def get_os():
+    """
+    Get the current Operating System.
+    """
+    return platform.system().lower().split('_').pop(0)
 
-env_vars = []
 
-for key in Config.options(system):
-    env_vars.append('export %s=%s' % (key.upper(), Config.get(system, key)))
+def load_firefox_paths(in_file, out_file):
+    """
+    Load Firefox configs from an input .INI file and add them as exportable
+    environment variables in a new file.
+    """
+    env_vars = []
+    system = get_os()
 
-with open('.env', 'w') as f:
-    f.write('\n'.join(env_vars))
+    if os.path.isfile(out_file):
+        os.remove(out_file)
+
+    if not os.path.isfile(in_file):
+        print 'Config file not found: %s' % (in_file)
+        exit(1)
+
+    Config = ConfigParser.ConfigParser()
+    Config.read(in_file)
+
+    try:
+        for key in Config.options(system):
+            value = Config.get(system, key)
+            env_vars.append('export %s=%s' % (key.upper(), value))
+    except:
+        print 'Unable to find config for "%s" in %s' % (system, in_file)
+        print 'Valid environments are: %s' % (', '.join(Config.sections()))
+        exit(1)
+
+    with open(out_file, 'w') as f:
+        f.write('\n'.join(env_vars))
+
+
+load_firefox_paths('path_firefox.ini', '.env')
