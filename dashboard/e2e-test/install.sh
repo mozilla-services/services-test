c2d55306d6c720c68e79835ab991d4c33b7b3b00
Stuart Philp <stuartphilp@gmail.com>
2015-11-27 14:06:56 -0500

    quick fix for dashboard deploy to run dashing in background

--- dashboard/e2e-test/install.sh
+++ dashboard/e2e-test/install.sh
@@ -5,5 +5,6 @@ rm -rf services-quality-dashboard
 git clone http://github.com/mozilla-services/services-quality-dashboard
 cd services-quality-dashboard
 git checkout $1
+# mkdir -p services-quality-dashboard/tmp/pids/
 bundle install
-dashing start
+dashing start -d
