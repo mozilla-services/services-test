093d8262287ff254a222758aba38b0109afa4f30
Stuart Philp <stuartphilp@gmail.com>
2015-11-27 16:25:37 -0500

    use the proper hostname for the controller

--- dashboard/e2e-test/deploy.sh
+++ dashboard/e2e-test/deploy.sh
@@ -1,4 +1,4 @@
 #push the docker image that was built prior to testing
 docker push mozservicesqa/dashboard
 # run vagrant provision on the controller to get the new docker image deployed
-ssh -i ~/.ssh/services-qa-jenkins.pem ubuntu@52.91.46.204 "cd services-qa-utils; vagrant provision services-qa-dashboard;"
+ssh -i ~/.ssh/services-qa-jenkins.pem ubuntu@services-qa-jenkins-controller.stage.mozaws.net "cd services-qa-utils; vagrant provision services-qa-dashboard;"
