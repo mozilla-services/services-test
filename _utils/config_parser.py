98ff459dac4a3e2d2f06cb9b7033c988f1ea414f
Peter deHaan <peter@deseloper.com>
2015-11-30 15:22:27 -0800

    Actually download files and do some basic file i/o

--- _utils/config_parser.py
+++ _utils/config_parser.py
@@ -3,7 +3,7 @@
 import os
 import platform
 import shutil
-import urllib
+import urllib2
 import ConfigParser
 
 DOWNLOAD_TMP_FOLDER = ".tmp"
@@ -16,15 +16,15 @@ def get_os():
     return platform.system().lower().split("_").pop(0)
 
 
-def load_config():
+def load_config(file=get_os()):
     """
     Load an INI config filed based on operating system.
     """
-    os_config = "configs/%s.ini" % (get_os())
+    os_config = "configs/%s.ini" % (file)
 
     # Fail fast if the input INI config file isn't found for current OS.
     if not os.path.isfile(os_config):
-        print("Config file not found for specified OS: %s" % (get_os()))
+        print("Config file not found for specified OS: %s" % (file))
         exit(1)
 
     # Load and return the config parser.
@@ -33,26 +33,51 @@ def load_config():
     return config
 
 
-def download_firefoxes(config):
+def download_firefoxes(config, tmp_folder=DOWNLOAD_TMP_FOLDER):
     """
     Download Firefox archives for all the specified channels.
     """
     header("DOWNLOAD FIREFOXES (%s)" % (get_os()))
 
-    # If the temp folder already exists, forcibly remove it.
-    if os.path.isdir(DOWNLOAD_TMP_FOLDER):
-        shutil.rmtree(DOWNLOAD_TMP_FOLDER)
-
-    # Create the temp directory before downloading files.
-    os.makedirs(DOWNLOAD_TMP_FOLDER)
+    # Delete and recreate the temp folder if necessary.
+    clean_tmp(tmp_folder)
 
     # Loop over each section and download the latest OS specific binaries.
     for section in config.sections():
         download_filename = config.get(section, "DOWNLOAD_FILENAME")
         download_url = config.get(section, "DOWNLOAD_URL")
-        download_path = os.path.join(DOWNLOAD_TMP_FOLDER, download_filename)
-        print "Downloading [%s]... %s" % (section, download_url)
-        urllib.urlretrieve(download_url, download_path)
+        out_file = os.path.join(tmp_folder, download_filename)
+
+        print("Downloading [%s]... %s" % (section, download_url))
+        download_file_from_url(download_url, out_file)
+
+
+def download_file_from_url(download_url, out_file):
+    """
+    Download a remote file and save it locally.
+    """
+    with open(out_file, "wb") as f:
+        try:
+            contents = urllib2.urlopen(download_url)
+            f.write(contents.read())
+        except urllib2.HTTPError as err:
+            # TODO: Do we need to fail harder here if we're unable to download
+            # the specified file? Currently it will silently fail and proceed.
+            print("\t", err)
+
+        f.close()
+
+
+def clean_tmp(tmp_folder):
+    """
+    rm -rf everything!
+    """
+    # If the specified folder already exists, forcibly remove it.
+    if os.path.isdir(tmp_folder):
+        shutil.rmtree(tmp_folder)
+
+    # Create the specified directory before downloading files.
+    os.makedirs(tmp_folder)
 
 
 def create_env_file(config, out_file):
@@ -84,7 +109,7 @@ def create_env_file(config, out_file):
 
 def header(str):
     """
-    Draw a pretty header.
+    Draw a pretty header for easier debugging.
     """
     divider = "=" * 60
     print("\n\n%s\n%s\n%s" % (divider, str, divider))
