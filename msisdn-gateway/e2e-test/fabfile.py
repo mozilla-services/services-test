966d3ee344c0c2504da6728efe9bd12aae0747b8
Peter deHaan <peter@deseloper.com>
2016-01-05 10:38:00 -0800

    Fixing flake8 and eslint errors/warnings

--- msisdn-gateway/e2e-test/fabfile.py
+++ msisdn-gateway/e2e-test/fabfile.py
@@ -1,9 +1,11 @@
-from fabric.api import *
-import fabric
+from fabric.api import *  # noqa
+# import fabric
+
 
 @task
 def setup():
     local("git clone https://github.com/kreamkorokke/msisdn-cli")
+
 
 @task
 def teardown():
