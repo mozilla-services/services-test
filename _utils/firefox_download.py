9ee90819e64c8860e86cc9b4779b4ce4ead78ac0
Richard Pappalardo <rpappalax@gmail.com>
2016-02-18 15:41:59 -0800

    flake8

--- _utils/firefox_download.py
+++ _utils/firefox_download.py
@@ -1,14 +1,11 @@
 """
 Module to download OS-specific versions of Firefox:
 1. General Release (gr)
-2. Beta (beta) 
+2. Beta (beta)
 3. Developer Edition (aurora)
-4. Nightly (nightly) 
+4. Nightly (nightly)
 """
 
-#!/usr/bin/env python
-
-import os
 from firefox_env_handler import IniHandler
 from mozdownload import FactoryScraper
 
@@ -25,28 +22,31 @@ config.read(CONFIG_CHANNELS)
 env = IniHandler()
 env.load_os_config('configs')
 
+
 def set_channel(channel):
     ch_version = config.get(channel, 'version')
     ch_type = config.get(channel, 'type')
     ch_branch = config.get(channel, 'branch')
     return ch_version, ch_type, ch_branch
 
+
 def download(channel):
     print('USING CHANNEL: {0}'.format(channel))
     v, t, b = set_channel(channel)
     download_filename = env.get(channel, 'DOWNLOAD_FILENAME')
     scraper = FactoryScraper(
-        t, 
-        version=v, 
-        branch=b, 
+        t,
+        version=v,
+        branch=b,
         destination='_temp/{0}'.format(download_filename)
     )
-    filename = scraper.download()
+    scraper.download()
+
 
 def main():
     for channel in config.sections():
         download(channel)
 
+
 if __name__ == '__main__':
     main()
-
