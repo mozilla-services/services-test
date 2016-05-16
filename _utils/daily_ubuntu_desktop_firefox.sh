7a84ef7eb1a0f47bcb4a15c83f1358a0a8dc49ce
Richard Pappalardo <rpappalax@gmail.com>
2016-05-16 16:47:27 -0700

    set firefox default to nightly

--- _utils/daily_ubuntu_desktop_firefox.sh
+++ _utils/daily_ubuntu_desktop_firefox.sh
@@ -32,7 +32,7 @@ ln -sf "${HOME_JENKINS}/firefoxes/firefox-nightly/firefox" firefox-nightly
 ln -sf "${HOME_JENKINS}/firefoxes/firefox-developer-edition/firefox" firefox-developer-edition
 ln -sf "${HOME_JENKINS}/firefoxes/firefox-beta/firefox" firefox-beta
 ln -sf "${HOME_JENKINS}/firefoxes/firefox-release/firefox" firefox-release
-ln -sf "${HOME_JENKINS}/firefox-release" firefox
+ln -sf "${HOME_JENKINS}/firefox-nightly" firefox
 
 echo  "--------------------------"
 echo "ADD FIREFOXES TO PATH"
