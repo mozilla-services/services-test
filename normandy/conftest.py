4b3bd6f56e5dc4c80b091b1352d0d296cc7f921b
Chris Hartjes <chartjes@littlehart.net>
2016-07-07 16:50:37 -0400

    Initial run of schema-check tests for Normandy's API

--- normandy/conftest.py
+++ normandy/conftest.py
@@ -6,11 +6,6 @@ TIMEOUT = 20
 
 def pytest_addoption(parser):
     parser.addoption(
-        '--bin',
-        default='/Applications/FirefoxBeta.app/Contents/MacOS/firefox-bin',
-        help='path for Firefox binary',
-    )
-    parser.addoption(
         "--env",
         action="store",
         required=True,
--- normandy/schema-check/test_api.py
+++ normandy/schema-check/test_api.py
@@ -1,4 +1,5 @@
 import configparser
+import jsonschema
 import pytest
 import requests
 
@@ -78,14 +79,72 @@ def filter_show_heartbeat(record):
     return record['name'] == 'show-heartbeat'
 
 
-def test_actions(conf, env):
+def filter_console_log(record):
+    return record['name'] == 'console-log'
+
+
+def test_expected_action_types(conf, env):
     r = requests.get(conf.get(env, 'api_root') + '/action')
     response = r.json()
 
     # Verify we have at least one response and then grab the first record
     assert len(response) >= 1
-    record = response[0]
 
+    # Make sure we only have expected action types
+    expected_records = ['console-log', 'show-heartbeat']
+
+    for record in response:
+        assert record['name'] in expected_records
+
+
+def test_console_log(conf, env):
+    r = requests.get(conf.get(env, 'api_root') + '/action')
+    response = r.json()
+
+    # Verify we have at least one response and then grab the first record
+    assert len(response) >= 1
+
+    # Look for any console-log actions
+    cl_records = list(filter(filter_console_log, response))
+
+    if len(cl_records) == 0:
+        pytest.skip('No console-log actions found')
+    else:
+        console_log_test(cl_records[0])
+
+
+def test_show_heartbeat(conf, env):
+    r = requests.get(conf.get(env, 'api_root') + '/action')
+    response = r.json()
+
+    # Verify we have at least one response and then grab the first record
+    assert len(response) >= 1
+
+    # Let's find at least one record that is a 'show-heartbeat'
+    sh_records = list(filter(filter_show_heartbeat, response))
+
+    if len(sh_records) == 0:
+        pytest.skip('No show-heartbeat actions found')
+    else:
+        show_hearbeat_test(sh_records[0])
+
+
+def show_hearbeat_test(record):
+    expected_action_fields = [
+        'name',
+        'implementation_url',
+        'arguments_schema'
+    ]
+    for field in record:
+        assert field in expected_action_fields
+
+    # Do we have a valid schema for 'arguments_schema'?
+    r = requests.get(record['arguments_schema']['$schema'])
+    schema = r.json()
+    assert jsonschema.validate(record['arguments_schema'], schema) is None
+
+
+def console_log_test(record):
     # Does an 'action' have all the required fields?
     expected_action_fields = [
         'name',
@@ -95,191 +154,7 @@ def test_actions(conf, env):
     for field in record:
         assert field in expected_action_fields
 
-    # Do we have the correct 'arguments_schema' fields?
-    expected_arguments_schema_fields = [
-        'required',
-        'title',
-        '$schema',
-        'description',
-        'type',
-        'properties'
-    ]
-    for field in record['arguments_schema']:
-        assert field in expected_arguments_schema_fields
-
-    # Is 'arguments_schema' built correctly?
-    assert 'message' in record['arguments_schema']['required']
-
-    # Are properties build correctly
-    expected_properties_fields = [
-        'description',
-        'type',
-        'default'
-    ]
-    for field in record['arguments_schema']['properties']['message']:
-        assert field in expected_properties_fields
-
-    # Let's find at least one record that is a 'show-heartbeat'
-    sh_records = list(filter(filter_show_heartbeat, response))
-
-    if len(sh_records) > 0:
-        show_hearbeat_test(sh_records[0])
-    else:
-        assert False
-
-
-def show_hearbeat_test(sh_record):
-    expected_action_fields = [
-        'name',
-        'implementation_url',
-        'arguments_schema'
-    ]
-    for field in sh_record:
-        assert field in expected_action_fields
-
-    # Look for our show-heartbeat specific arguments_schema fields
-    expected_sh_arguments_schema_fields = [
-        'description',
-        'title',
-        '$schema',
-        'required',
-        'type',
-        'properties',
-        'definitions'
-    ]
-    for field in sh_record['arguments_schema']:
-        assert field in expected_sh_arguments_schema_fields
-
-    # Let's check for the properties associated with heartbeats
-    expected_sh_properies = [
-        'defaults',
-        'surveyId',
-        'surveys'
-    ]
-
-    # Do we have our expected show-hearbeat properties?
-    for field in sh_record['arguments_schema']['properties']:
-        assert field in expected_sh_properies
-
-    # Check that our properties look as expected
-    properties = sh_record['arguments_schema']['properties']
-
-    # Are the default fields there?
-    expected_sh_properties_defaults = [
-        'description',
-        'title',
-        'propertyOrder',
-        '$ref'
-    ]
-    for field in properties['defaults']:
-        assert field in expected_sh_properties_defaults
-
-    # Are the surveyId fields there as expected?
-    expected_sh_properties_surveyId = [
-        'description',
-        'type',
-        'propertyOrder'
-    ]
-    for field in properties['surveyId']:
-        assert field in expected_sh_properties_surveyId
-
-    # Are the survey fields there as expected?
-    expected_sh_properties_surveys = [
-        'minItems',
-        'format',
-        'type',
-        'items',
-        'propertyOrder'
-    ]
-    for field in properties['surveys']:
-        assert field in expected_sh_properties_surveys
-
-    # Look for expected item fields inside the surveys
-    expected_survey_items_fields = [
-        '$ref',
-        'headerTemplate'
-    ]
-    for field in properties['surveys']['items']:
-        assert field in expected_survey_items_fields
-
-    # Do we have the fields we expect in our definitions?
-    definitions = sh_record['arguments_schema']['definitions']
-
-    expected_definitions = [
-        'message',
-        'title',
-        'postAnswerUrl',
-        'survey',
-        'engagementButtonLabel',
-        'thanksMessage',
-        'learnMoreUrl',
-        'learnMoreMessage',
-        'weightedSurvey'
-    ]
-    for field in definitions:
-        assert field in expected_definitions
-
-    # Some fields are standardized on description, type, default
-    default_fields = [
-        'description',
-        'type',
-        'default'
-    ]
-    standard_fields = [
-        'message',
-        'title',
-        'postAnswerUrl',
-        'engagementButtonLabel',
-        'thanksMessage',
-        'learnMoreUrl',
-        'learnMoreMessage'
-    ]
-    for field in standard_fields:
-        for key in default_fields:
-            assert key in definitions[field]
-
-    # Defined surveys have some standard fields as well
-    expected_definition_survey_fields = [
-        'type',
-        'properties'
-    ]
-    expected_definition_survey_properties_fields = [
-        '$ref',
-        'propertyOrder'
-    ]
-    for field in expected_definition_survey_fields:
-        assert field in definitions['survey']
-
-    for x in definitions['survey']['properties']:
-        for key in definitions['survey']['properties'][x]:
-            assert key in expected_definition_survey_properties_fields
-
-    # Check that our 'weightedSurvey' fields all make sense
-    weighted_survey = definitions['weightedSurvey']
-    property_weight_fields = [
-        'minimum',
-        'description',
-        'type',
-        'default',
-        'propertyOrder'
-    ]
-    property_title_fields = [
-        '$ref',
-        'propertyOrder'
-    ]
-    assert weighted_survey['allOf']
-
-    # Make sure our various expected fields exist
-    for item in weighted_survey['allOf']:
-        if '$ref' in item:
-            assert True
-        elif 'properties' in item:
-            for key in item['properties']['weight']:
-                assert key in property_weight_fields
-            for key in item['properties']['title']:
-                assert key in property_title_fields
-        elif 'required' in item:
-            if 'weight' in item:
-                assert True
-        else:
-            assert False
+    # Do we have a valid schema for 'arguments_schema'?
+    r = requests.get(record['arguments_schema']['$schema'])
+    schema = r.json()
+    assert jsonschema.validate(record['arguments_schema'], schema) is None
