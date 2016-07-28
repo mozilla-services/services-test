77d6585e665233b1d40dc6c5483d9bd684391f6d
jrbenny35 <bennyjr169@gmail.com>
2016-07-28 09:54:35 -0400

    Pep8 corrections and added some comments

--- opengraph/dom-test/conftest.py
+++ opengraph/dom-test/conftest.py
@@ -3,9 +3,12 @@ import pytest
 from bs4 import BeautifulSoup
 
 
+# ommand line parser
 def pytest_addoption(parser):
-    parser.addoption("--url", action="store", default="mozilla.org",
+    parser.addoption(
+        "--url", action="store", default="mozilla.org",
         help="Url for testing")
+
 
 # Load url and parse it
 @pytest.fixture
@@ -14,16 +17,19 @@ def meta(response_obj):
     return soup.find_all("meta")
 
 
+# Recieved url and input into requests
 @pytest.fixture
 def response_obj(url):
     return requests.get('http://' + url)
 
 
+# Recieve url from command line
 @pytest.fixture
 def url(request):
     return request.config.getoption("--url")
 
 
+# Required tags list
 @pytest.fixture
 def required_tags():
     og_required = [
@@ -35,6 +41,7 @@ def required_tags():
     return og_required
 
 
+# Empty list for tags found
 @pytest.fixture
 def found_tags():
     return []
