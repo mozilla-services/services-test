d9ebb347b2ce2378fa41d8d75564aa6b48934da0
Chris Hartjes <chartjes@littlehart.net>
2016-09-20 10:58:44 -0400

    Temp commit

--- kinto/config-check-test/test_kinto_signer.py
+++ kinto/config-check-test/test_kinto_signer.py
@@ -2,27 +2,37 @@ import ConfigParser
 import pytest
 from kinto_signer.serializer import canonical_json
 from kinto_signer.signer.local_ecdsa import ECDSASigner
-from kinto_http import Client
+from kinto_http import Client, KintoException
 from pytest_testrail.plugin import testrail
 
 
 @pytest.fixture
-def conf(request):
+def conf():
     config = ConfigParser.ConfigParser()
     config.read('manifest.ini')
     return config
 
 
 def verify_signatures(client):
-    dest_col = client.get_collection()
-    records = client.get_records(_sort='-last_modified')
-    timestamp = client.get_records_timestamp()
-    serialized = canonical_json(records, timestamp)
-    signature = dest_col['data']['signature']
-    with open('pub', 'w') as f:
-        f.write(signature['public_key'])
-    signer = ECDSASigner(public_key='pub')
-    return signer.verify(serialized, signature) is None
+    """
+    If we get an exception we need to check the HTTP status code. If it's a 401
+    it's the result of the collection not existing, otherwise it should be a
+    failure
+    """
+    try:
+        dest_col = client.get_collection()
+        records = client.get_records(_sort='-last_modified')
+        timestamp = client.get_records_timestamp()
+        serialized = canonical_json(records, timestamp)
+        signature = dest_col['data']['signature']
+        with open('pub', 'w') as f:
+            f.write(signature['public_key'])
+        signer = ECDSASigner(public_key='pub')
+        return signer.verify(serialized, signature) is None
+    except KintoException as e:
+        if e.response.status_code == 401:
+            return -1
+        return 0
 
 
 @testrail('C5478')
@@ -32,7 +42,12 @@ def test_addons_signatures(env, conf):
         bucket='blocklists',
         collection='addons'
     )
-    assert verify_signatures(client)
+    response = verify_signatures(client)
+
+    if response != -1:
+        assert response
+    else:
+        pytest.skip('Addons collection does not exist')
 
 
 @testrail('C5475')
@@ -42,7 +57,12 @@ def test_plugins_signatures(env, conf):
         bucket='blocklists',
         collection='plugins'
     )
-    assert verify_signatures(client)
+    response = verify_signatures(client)
+
+    if response != -1:
+        assert response
+    else:
+        pass
 
 
 @testrail('C5476')
@@ -52,7 +72,12 @@ def test_gfx_signatures(env, conf):
         bucket='blocklists',
         collection='gfx'
     )
-    assert verify_signatures(client)
+    response = verify_signatures(client)
+
+    if response != -1:
+        assert response
+    else:
+        pass
 
 
 @testrail('C5477')
@@ -62,4 +87,9 @@ def test_certificates_signatures(env, conf):
         bucket='blocklists',
         collection='certificates'
     )
-    assert verify_signatures(client)
+    response = verify_signatures(client)
+
+    if response != -1:
+        assert response
+    else:
+        pass
